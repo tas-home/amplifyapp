6c60f15738fea6e15053792195609695bf672060
tas-home <tas.home@outlook.jp>
2021-05-16 11:11:32 +0000

    add image function

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://pi72ghogenafxersw7ozt6uxja.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-qbrty7rcxrdb3lhtpclpatjugy"
+    "aws_appsync_apiKey": "da2-qbrty7rcxrdb3lhtpclpatjugy",
+    "aws_user_files_s3_bucket": "memoapp-bucket-demo52612-dev",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
